b291763059f3689696f4ca043a75294f1d647b99
Horacio López <74926954+Lopez19@users.noreply.github.com>
2022-10-18 01:21:28 -0500

    Add Scroll, Active-link (Header) V2

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,9 +24,45 @@ function linkAction() {
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+   const scrollY = window.pageYOffset;
+
+   sections.forEach((current) => {
+      const sectionHeight = current.offsetHeight,
+         sectionTop = current.offsetTop - 50,
+         sectionId = current.getAttribute("id");
+
+      if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+         document
+            .querySelector(".nav__menu a[href*=" + sectionId + "]")
+            .classList.add("active-link");
+      } else {
+         document
+            .querySelector(".nav__menu a[href*=" + sectionId + "]")
+            .classList.remove("active-link");
+      }
+   });
+}
+window.addEventListener("scroll", scrollActive);
 
 /*=============== CAMBIAR BACKGROUND HEADER ===============*/
+function scrollHeader() {
+   const nav = document.getElementById("header");
+   // Cuando el desplazamiento sea superior a 80vh (altura de la ventana), añada la clase scroll-header a la etiqueta de cabecera.
+   if (this.scrollY >= 80) nav.classList.add("scroll-header");
+   else nav.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
+   const scrollUp = document.getElementById("scroll-up");
+   //Cuando el desplazamiento sea superior a 560vh (altura de la ventana), añada la clase show-scroll a la etiqueta a con la clase scroll-top.
+   if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+   else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
 
 /*=============== MODO OSCURO ===============*/
